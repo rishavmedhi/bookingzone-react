dcf11c00cf3c2421922fe3ac4cc805095bc7f56d
Rishav Medhi <rishav164@gmail.com>
2019-11-14 01:18:13 +0530

    Hid the default navbar of react router and installed bootstrap for css

--- src/App.js
+++ src/App.js
@@ -11,22 +11,22 @@ export default function App() {
   return (
       <Router>
         <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/users">Users</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-            </ul>
-          </nav>
+          {/*<nav>*/}
+          {/*  <ul>*/}
+          {/*    <li>*/}
+          {/*      <Link to="/">Home</Link>*/}
+          {/*    </li>*/}
+          {/*    <li>*/}
+          {/*      <Link to="/about">About</Link>*/}
+          {/*    </li>*/}
+          {/*    <li>*/}
+          {/*      <Link to="/users">Users</Link>*/}
+          {/*    </li>*/}
+          {/*    <li>*/}
+          {/*      <Link to="/login">Login</Link>*/}
+          {/*    </li>*/}
+          {/*  </ul>*/}
+          {/*</nav>*/}
 
           {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
