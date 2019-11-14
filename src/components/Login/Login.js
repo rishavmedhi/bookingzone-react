84d239a34fc56f17263fc459ebe5a810c7a81c2c
Rishav Medhi <rishav164@gmail.com>
2019-11-14 19:28:32 +0530

    Created onchange listeners for the login fields

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,6 +3,32 @@ import './style.css'
 
 class Login extends React.Component
 {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            username: "",
+            password: ""
+        };
+
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleUsernameChange(e){
+        this.setState({username : e.target.value});
+    }
+
+    handlePasswordChange(e){
+        this.setState({password: e.target.value});
+    }
+
+    handleSubmit(event) {
+
+    }
+
+
 
     render(){
         return(
@@ -13,13 +39,13 @@ class Login extends React.Component
                 </div>
 
                 <div className="login_form_wrapper">
-                    <form>
+                    <form onSubmit={this.handleSubmit}>
                         <h4 className='form_heading'>Login</h4>
                         <div className="form-group">
-                            <input id="username" type="text" className="form-control" placeholder="Enter username" required/>
+                            <input id="username" type="text" className="form-control" placeholder="Enter username" required onChange={this.handleUsernameChange}/>
                         </div>
                         <div className="form-group">
-                            <input id="password" type="password" className="form-control" placeholder="Enter password" required/>
+                            <input id="password" type="password" className="form-control" placeholder="Enter password" required onChange={this.handlePasswordChange}/>
                         </div>
                         <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
