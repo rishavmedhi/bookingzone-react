39a7ac784148d75b276b05b8b98249be3fdf60e1
Rishav Medhi <rishav164@gmail.com>
2019-11-14 23:34:32 +0530

    Adding elements in the navbar

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom'
-// import './style.css'
+import './style.css'
 import axios from 'axios';
 import Login from "../Login/Login";
 
@@ -13,9 +13,20 @@ class Dashboard extends React.Component
 
     render() {
         return(
-            <nav className="navbar navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <span className="navbar-brand mb-0 h1">BookingZone</span>
-                
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end">
+                    <div className="navbar-nav">
+                        <div className="nav-item nav-link">My Bookings</div>
+                        <div className="nav-item nav-link">Logout</div>
+                    </div>
+                </div>
+
             </nav>
 
 
