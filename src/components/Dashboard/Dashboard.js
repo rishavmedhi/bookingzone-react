a1d31fe06885caf3164b9830a614ec78fa156073
Rishav Medhi <rishav164@gmail.com>
2019-11-15 00:26:17 +0530

    Minimum skeleton for dashboard form

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -13,21 +13,85 @@ class Dashboard extends React.Component
 
     render() {
         return(
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <span className="navbar-brand mb-0 h1">BookingZone</span>
-                <button className="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse justify-content-end">
-                    <div className="navbar-nav">
-                        <div className="nav-item nav-link">My Bookings</div>
-                        <div className="nav-item nav-link">Logout</div>
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <span className="navbar-brand mb-0 h1">BookingZone</span>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-end">
+                        <div className="navbar-nav">
+                            <div className="nav-item nav-link">My Bookings</div>
+                            <div className="nav-item nav-link">Logout</div>
+                        </div>
+                    </div>
+                </nav>
+                <div className="dashboard_container container">
+                    <div className="col align-self-center justify-content-center">
+                        <h3>What would you like to book today?</h3>
+                    </div>
+
+                    <div className="booking_wrapper">
+                        <form>
+                            <div className="activity_wrapper">
+                                <h4>Activities </h4>
+                                <div className="activity_select_wrapper">
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="radio" name="activity" value="TENNIS" checked />
+                                        <label className="form-check-label">Tennis Court</label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="radio" name="activity" value="SWIMMING" />
+                                        <label className="form-check-label">Swimming Pool</label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="radio" name="activity" value="BADMINTON" />
+                                        <label className="form-check-label">Badminton</label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="radio" name="activity" value="GYM" />
+                                        <label className="form-check-label">Gym</label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="radio" name="activity" value="CLUBHOUSE" />
+                                        <label className="form-check-label">Club House</label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="radio" name="activity" value="CYCLE" />
+                                        <label className="form-check-label">Cycle Track</label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="time_selector_wrapper">
+                                <h4>Day and Time</h4>
+                                <div className="time_selector">
+                                    <div className="form-group">
+                                        <label htmlFor="booking_day">Booking Day</label>
+                                        <input className="form-control" type="date" id="booking_day" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="starttime">Start Time</label>
+                                        <input className="form-control" type="time" id="starttime" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="endtime">End Time</label>
+                                        <input className="form-control" type="time" id="endtime" />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="confirm_booking_wrapper">
+                                <button type="submit" className="btn btn-primary booking_submit">Confirm Booking</button>
+                            </div>
+
+                        </form>
                     </div>
                 </div>
+            </div>
 
-            </nav>
 
 
         );
