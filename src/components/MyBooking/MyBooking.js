dc8a2890150a9aa9b171969204dd76a83e68912a
Rishav Medhi <rishav164@gmail.com>
2019-11-16 01:12:14 +0530

    Converting upcoming bookings into functional components

--- src/components/MyBooking/MyBooking.js
+++ src/components/MyBooking/MyBooking.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+import './style.css'
+
 class MyBooking extends React.Component{
     constructor(props)
     {
@@ -46,13 +48,95 @@ class MyBooking extends React.Component{
                </nav>
 
                <div className="mybooking_container container">
+                    {/*<div className="upcoming_bookings_wrapper">*/}
+                    {/*    <h3>Upcoming Bookings</h3>*/}
+                    {/*    <div class="booking_wrapper" data-booking-id="1234">*/}
+                    {/*        <div className="event">Event : <span className="event_name">TENNIS</span></div>*/}
+                    {/*        <div className="day_wrapper">Day : <span className="day">16th November 2019 </span></div>*/}
+                    {/*        <div className="time_wrapper">Time : <span className="time"><span className="starttime">9:00 A.M.</span> To <span className="endtime">10:00 A.M.</span></span></div>*/}
+                    {/*        <div className="cancel_booking float-right">*/}
+                    {/*            x Cancel Booking*/}
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*    <div className="booking_wrapper" data-booking-id="1234">*/}
+                    {/*        <div className="event">Event : <span className="event_name">TENNIS</span></div>*/}
+                    {/*        <div className="day_wrapper">Day : <span className="day">16th November 2019 </span></div>*/}
+                    {/*        <div className="time_wrapper">Time : <span className="time"><span className="starttime">9:00 A.M.</span> To <span*/}
+                    {/*            className="endtime">10:00 A.M.</span></span></div>*/}
+                    {/*        <div className="cancel_booking float-right">*/}
+                    {/*            x Cancel Booking*/}
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
+                    <UpcomingBooking bookings={this.state.bookings}/>
 
+                   <div className="past_bookings">
+                       <h3>Past Bookings</h3>
+                       <div className="booking_wrapper past" data-booking-id="1234">
+                           <div className="event">Event : <span className="event_name">TENNIS</span></div>
+                           <div className="day_wrapper">Day : <span className="day">16th November 2019 </span></div>
+                           <div className="time_wrapper">Time : <span className="time"><span className="starttime">9:00 A.M.</span> To <span
+                               className="endtime">10:00 A.M.</span></span></div>
+                       </div>
+                   </div>
                </div>
            </div>
-
-
         )
     }
 }
+
+function UpcomingBooking(props)
+{
+    let currenttime = Math.floor((new Date().getTime())/1000);
+    let rows=[];
+    for(let i=0;i<props.bookings.length;i++)
+    {
+        let booking = props.bookings[i];
+        console.log(booking);
+        if(booking.starttime>currenttime)
+        {
+            rows.push(<Booking past="" booking_id={booking._id} event={booking.event} day={new Date(booking.starttime).toDateString()} starttime={new Date(booking.starttime).toLocaleTimeString()} endtime={new Date(booking.endtime).toTimeString()}/>)
+        }
+    }
+    return(
+        <div className="upcoming_bookings_wrapper">
+            <h3>Upcoming Bookings</h3>
+            {rows}
+        </div>
+    );
+}
+
+function Booking(props)
+{
+    return(
+        <div className={'booking_wrapper '+props.past} data-booking-id={props.booking_id}>
+            <div className="event">Event : <span className="event_name">{props.event}</span></div>
+            <div className="day_wrapper">Day : <span className="day">{props.day} </span></div>
+            <div className="time_wrapper">Time : <span className="time"><span className="starttime">{props.starttime}</span> To <span
+                className="endtime">{props.endtime}</span></span>
+            </div>
+            {CancelRequired(props.past)}
+        </div>
+    );
+}
+
+function CancelBooking(props)
+{
+    return(
+        <div className="cancel_booking float-right">
+            x Cancel Booking
+        </div>
+    )
+}
+
+function CancelRequired(isPast)
+{
+    if(!isPast) {
+        return (
+            <CancelBooking/>
+        )
+    }
+}
+
 
 export default MyBooking;
